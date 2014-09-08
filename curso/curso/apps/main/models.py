1db4e901160e9f89629e7c28930ec267e898e89a
texto <yampier--07@hotmail.com>
2014-09-08 16:56:01 -0500

    Agregada la clase de Perfil de Usuario

--- curso/curso/apps/main/models.py
+++ curso/curso/apps/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Perfil(models.Model):
+	usuario = models.OneToOneField(User)
+	telefono = models.CharField(max_length=20)
+	dereccion = models.TextField()
+	
+	def __unicode__(self):
+		return self.usuario.username
+	
+	class Meta:
+		verbose_name_plural = ('Perfiles')
--- curso/curso/settings.py
+++ curso/curso/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'curso.apps.main',
 )
 
 MIDDLEWARE_CLASSES = (
